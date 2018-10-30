ccf2d42a50b35774ab302c05ba4e54f1e2d315f1
Akash Shakdwipeea <ashakdwipeea@gmail.com>
2018-10-30 20:15:39 +0530

    improve print of class
    now we can print any class !!

--- links.lisp
+++ links.lisp
@@ -30,9 +30,15 @@
 	  :initial-value '()))
 
 
-(defun get-slots (class)
+;;;;;;;;;;;;;;;;
+;; CLOS utils ;;
+;;;;;;;;;;;;;;;;
+
+
+(defun get-slots (cls)
+  "get slots of a class"
   (mapcar #'closer-mop:slot-definition-name
-	  (closer-mop:class-direct-slots (class-of (make-instance class)))))
+	  (closer-mop:class-direct-slots (class-of (make-instance cls)))))
 
 
 (defun instance-from-plist (instance-type plist)
@@ -43,6 +49,23 @@
 							      (get-in plist slot))))
 					 (get-slots instance-type)
 					 :initial-value (list)))))
+
+
+(defvar slots (get-slots 'post))
+
+
+(defmacro enable-print (c)
+  "add a generic print method to class c which prints all the slots of the class"
+  (let* ((slots (get-slots c))
+	 (fmt (reduce (lambda (str slot)
+			  (concatenate 'string str " ~s " (symbol-name slot)))
+			slots
+			:initial-value "Value:")))
+    `(defmethod print-object ((object post) stream)
+       (print-unreadable-object (object stream :type t)
+	 (with-slots ,slots object
+	   (funcall #'format stream ,fmt ,@(mapcar (lambda (slot) (list 'print slot))  slots)))))))
+
 
 ;; (defconstant pl '(:abc "hello" :def "no"))
 
@@ -60,7 +83,7 @@
 (defclass post ()
   ((|subreddit| :col-type (:varchar 64)
 	      :initarg :subreddit
-	      :accessor post-subreddit)
+	      :accessor subreddit)
    (|url| :col-type (:varchar 256)
 	:initarg :url)
    (|selftext| :col-type (:varchar 500)
@@ -72,35 +95,56 @@
    (|author| :col-type (:varchar 64)
 	   :initarg :author)
    (|title| :col-type (:varchar 256)
-	  :initarg :title)
-   (|created-utc| :col-type (:varchar 128)
-		:initarg :created-utc))
+	    :initarg :title
+	    :accessor :title)
+   (|created_utc| :col-type (:varchar 128)
+		  :initarg :created_utc
+		  :accessor created-utc))
   (:metaclass mito:dao-table-class))
 
-;; (defun make-post (plist)
-;;   "given a plist it will create a post by finding required elements from the plist"
-;;   (apply make-instance
-;; 	 (append (list 'post)
-;; 		 )))
+
+(enable-print post)
+
+
+(defun store-posts (posts)
+  (mapcar #'mito:insert-dao posts))
+
 
 ;;;;;;;;;;;;;;;;;
 ;; Application ;;
 ;;;;;;;;;;;;;;;;;
 
-(post-subreddit (instance-from-plist 'post child-data))
+(defun create-tables ()
+  (mito:ensure-table-exists 'post))
 
-(type-of (car (get-slots 'post)))
-
-(get-in child-data :|title|)
 
 (defvar user-agent '("User-Agent" "Mozilla/5.0 (Windows NT 6.1;) Gecko/20100101 Firefox/13.0.1"))
 
 
+(defvar child-data '())
+
+;; child-data is a particular post in the subreddit json
+;; experments follow
+
+;; (getf child-data :|created_utc|)
+
+;; (created-utc (instance-from-plist 'post child-data))
+
+;; (type-of (car (get-slots 'post)))
+
+;; (get-in child-data :|title|)
+
+(defvar cren '())
+
 (defun parse-post-data (subreddit-json)
   (let ((children  (get-in subreddit-json "data" "children")))
+    (defvar cren children)
+    (format t "chidlren lengh ~d " (length children))
     (mapcar (lambda (child)
 	      (instance-from-plist 'post (get-in child "data")))
 	    children)))
+
+;; (mapcar length child-data)
 
 
 (defun fetch-subreddit-json (subreddit-name)
@@ -108,8 +152,18 @@
 					     "http://reddit.com/r/" subreddit-name ".json")
 				:headers (list user-agent))))
 
-(fetch-subreddit-json "clojure")
 
+(setq posts (fetch-subreddit-json "haskell"))
+
+(created-utc (second posts))
+
+(store-posts posts)
+
+
+
+;; connect and perform migrations
+(progn (connect)
+       (create-tables))
 
 ;; (assoc (intern "data" :keyword)  *res*)
 
